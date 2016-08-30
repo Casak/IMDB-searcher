c43ab987489c468ee571c715d483ac221bdbd30d
Casak123 <casak123@gmail.com>
2016-08-30 18:17:10 +0300

    TabsActivity refactor

--- app/src/main/java/ru/casak/IMDB_searcher/activities/TabsActivity.java
+++ app/src/main/java/ru/casak/IMDB_searcher/activities/TabsActivity.java
@@ -2,6 +2,7 @@ package ru.casak.IMDB_searcher.activities;
 
 import android.animation.ArgbEvaluator;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -15,30 +16,35 @@ import ru.casak.IMDB_searcher.R;
 import ru.casak.IMDB_searcher.adapters.TabWithFragmentPagerAdapter;
 
 public class TabsActivity extends AppCompatActivity {
-    private static final String TAG = "TabsActivity";
+    private static final String TAG = TabsActivity.class.getSimpleName();
+
     private static Context context;
+    private static TypedArray[] colorThemes;
+    private Window window;
+    private ArgbEvaluator colorEvaluator;
+
     private ViewPager viewPager;
     private TabLayout tabLayout;
 
-    private static TypedArray ColorSetTop250Tab;
-    private static TypedArray ColorSetComingSoonTab;
-    private static TypedArray ColorSetFavoriteTab;
-    private static TypedArray[] colorThemes;
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    private ArgbEvaluator colorEvaluator = new ArgbEvaluator();
-    private Window window;
+        context = getApplicationContext();
+        window = getWindow();
+
+        colorEvaluator = new ArgbEvaluator();
+
+        Resources resources = getResources();
+        colorThemes = new TypedArray[] {
+                resources.obtainTypedArray(R.array.colorSetTop250),
+                resources.obtainTypedArray(R.array.colorSetComingSoon),
+                resources.obtainTypedArray(R.array.colorSetFavorite)};
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        context = getApplicationContext();
-
-        ColorSetTop250Tab = getResources().obtainTypedArray(R.array.colorSetTop250);
-        ColorSetComingSoonTab = getResources().obtainTypedArray(R.array.colorSetComingSoon);
-        ColorSetFavoriteTab = getResources().obtainTypedArray(R.array.colorSetFavorite);
-        colorThemes = new TypedArray[] {ColorSetTop250Tab, ColorSetComingSoonTab, ColorSetFavoriteTab};
-        window = getWindow();
 
         setContentView(R.layout.activity_main);
 
@@ -46,43 +52,10 @@ public class TabsActivity extends AppCompatActivity {
         viewPager.setAdapter(new TabWithFragmentPagerAdapter(getSupportFragmentManager(),
                 TabsActivity.this));
         viewPager.setOffscreenPageLimit(2);
+        viewPager.addOnPageChangeListener(new ColorChangeListener());
 
         tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                TypedArray currentColorSet = colorThemes[position];
-                TypedArray nextColor = position < colorThemes.length - 1
-                        ? colorThemes[position + 1]
-                        : colorThemes[position];
-
-                Integer[] evalColor = new Integer[4];
-                for (int i = 0; i < 4; i++) {
-                    evalColor[i] = (Integer) colorEvaluator.evaluate(
-                            positionOffset,
-                            currentColorSet.getColor(i, 0),
-                            nextColor.getColor(i, 0)
-                    );
-                }
-
-                changeInterfaceHeadColorTheme(evalColor[0], evalColor[1], evalColor[2], evalColor[3]);
-                Log.d(TAG, "onPageScrolled() ----done");
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                changeColorTheme(colorThemes[position]);
-                Log.d(TAG, "onPageSelected() ----done");
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                Log.d(TAG, "onPageScrollStateChanged() ----done");
-            }
-        });
     }
 
     public static Context getContext() {
@@ -104,5 +77,38 @@ public class TabsActivity extends AppCompatActivity {
         window.setNavigationBarColor(backgroundColor);
         tabLayout.setBackgroundColor(backgroundColor);
         tabLayout.setSelectedTabIndicatorColor(tabIndicatorColor);
+    }
+
+    class ColorChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            TypedArray currentColorSet = colorThemes[position];
+            TypedArray nextColor = position < colorThemes.length - 1
+                    ? colorThemes[position + 1]
+                    : colorThemes[position];
+
+            Integer[] evalColor = new Integer[4];
+            for (int i = 0; i < 4; i++) {
+                evalColor[i] = (Integer) colorEvaluator.evaluate(
+                        positionOffset,
+                        currentColorSet.getColor(i, 0),
+                        nextColor.getColor(i, 0)
+                );
+            }
+
+            changeInterfaceHeadColorTheme(evalColor[0], evalColor[1], evalColor[2], evalColor[3]);
+            Log.d(TAG, "onPageScrolled() ----done");
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            changeColorTheme(colorThemes[position]);
+            Log.d(TAG, "onPageSelected() ----done");
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            Log.d(TAG, "onPageScrollStateChanged() ----done");
+        }
     }
 }
