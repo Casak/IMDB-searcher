f8c360f6488977d530bfa96b890ad3306c2db86f
Casak123 <casak123@gmail.com>
2016-08-31 17:06:48 +0300

    TabsActivity refactor

--- app/src/main/java/ru/casak/IMDB_searcher/activities/TabsActivity.java
+++ app/src/main/java/ru/casak/IMDB_searcher/activities/TabsActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.TabLayout;
@@ -19,42 +20,31 @@ public class TabsActivity extends AppCompatActivity {
     private static final String TAG = TabsActivity.class.getSimpleName();
 
     private static Context context;
-    private static TypedArray[] colorThemes;
-    private Window window;
-    private ArgbEvaluator colorEvaluator;
-
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        context = getApplicationContext();
-        window = getWindow();
-
-        colorEvaluator = new ArgbEvaluator();
-
-        Resources resources = getResources();
-        colorThemes = new TypedArray[] {
-                resources.obtainTypedArray(R.array.colorSetTop250),
-                resources.obtainTypedArray(R.array.colorSetComingSoon),
-                resources.obtainTypedArray(R.array.colorSetFavorite)};
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        context = getApplicationContext();
+
+        Resources resources = getResources();
+        TypedArray[] colors = new TypedArray[] {
+                resources.obtainTypedArray(R.array.colorSetTop250),
+                resources.obtainTypedArray(R.array.colorSetComingSoon),
+                resources.obtainTypedArray(R.array.colorSetFavorite)};
+
         setContentView(R.layout.activity_main);
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+
+        ColorChangeListener listener = new ColorChangeListener(colors, getWindow(), tabLayout);
+
         viewPager.setAdapter(new TabWithFragmentPagerAdapter(getSupportFragmentManager(),
                 TabsActivity.this));
         viewPager.setOffscreenPageLimit(2);
-        viewPager.addOnPageChangeListener(new ColorChangeListener());
+        viewPager.addOnPageChangeListener(listener);
 
-        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 
@@ -62,30 +52,25 @@ public class TabsActivity extends AppCompatActivity {
         return TabsActivity.context;
     }
 
-    private void changeColorTheme(TypedArray colorSet){
-        changeInterfaceHeadColorTheme(
-                colorSet.getColor(0, 0),
-                colorSet.getColor(1, 0),
-                colorSet.getColor(2, 0),
-                colorSet.getColor(3, 0)
-        );
-    }
-
-    private void changeInterfaceHeadColorTheme(Integer actionBarColor, Integer statusBarColor, Integer tabIndicatorColor, Integer backgroundColor) {
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(actionBarColor));
-        window.setStatusBarColor(statusBarColor);
-        window.setNavigationBarColor(backgroundColor);
-        tabLayout.setBackgroundColor(backgroundColor);
-        tabLayout.setSelectedTabIndicatorColor(tabIndicatorColor);
-    }
-
     class ColorChangeListener implements ViewPager.OnPageChangeListener {
+        private TypedArray[] colors;
+        private ArgbEvaluator colorEvaluator;
+        private Window window;
+        private TabLayout tabLayout;
+
+        public ColorChangeListener(TypedArray[] colors, Window window, TabLayout tabLayout){
+            this.colors = colors;
+            this.window = window;
+            this.tabLayout = tabLayout;
+            colorEvaluator = new ArgbEvaluator();
+        }
+
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            TypedArray currentColorSet = colorThemes[position];
-            TypedArray nextColor = position < colorThemes.length - 1
-                    ? colorThemes[position + 1]
-                    : colorThemes[position];
+            TypedArray currentColorSet = colors[position];
+            TypedArray nextColor = position < colors.length - 1
+                    ? colors[position + 1]
+                    : colors[position];
 
             Integer[] evalColor = new Integer[4];
             for (int i = 0; i < 4; i++) {
@@ -102,13 +87,38 @@ public class TabsActivity extends AppCompatActivity {
 
         @Override
         public void onPageSelected(int position) {
-            changeColorTheme(colorThemes[position]);
+            changeColorTheme(colors[position]);
             Log.d(TAG, "onPageSelected() ----done");
         }
 
         @Override
         public void onPageScrollStateChanged(int state) {
             Log.d(TAG, "onPageScrollStateChanged() ----done");
+        }
+
+        private void changeColorTheme(TypedArray colorSet) {
+            changeInterfaceHeadColorTheme(
+                    colorSet.getColor(0, 0),
+                    colorSet.getColor(1, 0),
+                    colorSet.getColor(2, 0),
+                    colorSet.getColor(3, 0)
+            );
+        }
+
+        private void changeInterfaceHeadColorTheme(Integer actionBarColor, Integer statusBarColor,
+                                                   Integer tabIndicatorColor, Integer backgroundColor) {
+
+            try{
+                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(actionBarColor));
+            } catch (NullPointerException e){
+                e.printStackTrace();
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.setStatusBarColor(statusBarColor);
+                window.setNavigationBarColor(backgroundColor);
+            }
+            tabLayout.setBackgroundColor(backgroundColor);
+            tabLayout.setSelectedTabIndicatorColor(tabIndicatorColor);
         }
     }
 }
