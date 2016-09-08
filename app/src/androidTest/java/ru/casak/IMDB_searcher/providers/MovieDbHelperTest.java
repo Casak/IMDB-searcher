7c7a0ad5da0b9da3caf7fb9cc4e03de056a2e9ba
Casak123 <casak123@gmail.com>
2016-09-08 15:20:22 +0300

    checkMovieTableColumns test refactor

--- app/src/androidTest/java/ru/casak/IMDB_searcher/providers/MovieDbHelperTest.java
+++ app/src/androidTest/java/ru/casak/IMDB_searcher/providers/MovieDbHelperTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.*;
 
 import org.mockito.Mock;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 
 public class MovieDbHelperTest {
@@ -29,7 +30,7 @@ public class MovieDbHelperTest {
 
     @BeforeClass
     public static void setUp() throws JSONException {
-        mContext =  new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "test_");
+        mContext = new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "test_");
         mSQLiteOpenHelper = new MovieDbHelper(mContext);
         //mMovieJSON = new JSONObject("{   \"adult\": false,   \"backdrop_path\": \"/hNFMawyNDWZKKHU4GYCBz1krsRM.jpg\",   \"belongs_to_collection\": null,   \"budget\": 63000000,   \"genres\": [     {       \"id\": 18,       \"name\": \"Drama\"     }   ],   \"homepage\": \"\",   \"id\": 550,   \"imdb_id\": \"tt0137523\",   \"original_language\": \"en\",   \"original_title\": \"Fight Club\",   \"overview\": \"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \\\"fight clubs\\\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.\",   \"popularity\": 2.50307202280779,   \"poster_path\": \"/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg\",   \"production_companies\": [     {       \"name\": \"20th Century Fox\",       \"id\": 25     },     {       \"name\": \"Fox 2000 Pictures\",       \"id\": 711     },     {       \"name\": \"Regency Enterprises\",       \"id\": 508     }   ],   \"production_countries\": [     {       \"iso_3166_1\": \"DE\",       \"name\": \"Germany\"     },     {       \"iso_3166_1\": \"US\",       \"name\": \"United States of America\"     }   ],   \"release_date\": \"1999-10-14\",   \"revenue\": 100853753,   \"runtime\": 139,   \"spoken_languages\": [     {       \"iso_639_1\": \"en\",       \"name\": \"English\"     }   ],   \"status\": \"Released\",   \"tagline\": \"How much can you know about yourself if you've never been in a fight?\",   \"title\": \"Fight Club\",   \"video\": false,   \"vote_average\": 7.7,   \"vote_count\": 3185 }");
     }
@@ -50,48 +51,34 @@ public class MovieDbHelperTest {
         Cursor cursor = testDB.rawQuery("SELECT name FROM sqlite_master where type = 'table'", null);
         assertTrue("Error: Database is not created correctly", cursor.moveToFirst());
 
-        do{
+        do {
             tableNames.remove(cursor.getString(0));
-        } while(cursor.moveToNext());
+        } while (cursor.moveToNext());
         assertTrue("Error: Database does not contain one or several tables", tableNames.isEmpty());
     }
 
     @Test
-    public void checkMovieTableColumns (){
+    public void checkMovieTableColumns() {
         final HashSet<String> columns = new HashSet<>();
-        columns.add(MovieContract.MovieEntry.COLUMN_ADULT);
-        columns.add(MovieContract.MovieEntry.COLUMN_BACKDROP_PATH);
-        columns.add(MovieContract.MovieEntry.COLUMN_BUDGET);
-        columns.add(MovieContract.MovieEntry.COLUMN_COMPANIES);
-        columns.add(MovieContract.MovieEntry.COLUMN_COUNTRIES);
-        columns.add(MovieContract.MovieEntry.COLUMN_GENRES);
-        columns.add(MovieContract.MovieEntry.COLUMN_HOMEPAGE);
-        columns.add(MovieContract.MovieEntry.COLUMN_IMDB_ID);
-        columns.add(MovieContract.MovieEntry.COLUMN_ORIGINAL_LANGUAGE);
-        columns.add(MovieContract.MovieEntry.COLUMN_ORIGINAL_TITLE);
-        columns.add(MovieContract.MovieEntry.COLUMN_OVERVIEW);
-        columns.add(MovieContract.MovieEntry.COLUMN_POPULARITY);
-        columns.add(MovieContract.MovieEntry.COLUMN_POSTER_PATH);
-        columns.add(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
-        columns.add(MovieContract.MovieEntry.COLUMN_REVENUE);
-        columns.add(MovieContract.MovieEntry.COLUMN_RUNTIME);
-        columns.add(MovieContract.MovieEntry.COLUMN_SPOKEN_LANGUAGES);
-        columns.add(MovieContract.MovieEntry.COLUMN_STATUS);
-        columns.add(MovieContract.MovieEntry.COLUMN_TAGLINE);
-        columns.add(MovieContract.MovieEntry.COLUMN_TITLE);
-        columns.add(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE);
-        columns.add(MovieContract.MovieEntry.COLUMN_VOTE_COUNT);
+        Field[] fields = MovieContract.MovieEntry.class.getDeclaredFields();
+        try {
+            for (Field f : fields)
+                if (f.getName().matches(".*(COLUMN_.+)"))
+                    columns.add((String) f.get(f));
+        } catch (IllegalAccessException e) {
+            fail("Can not retrieve columns from MovieEntry");
+        }
 
         SQLiteDatabase testDb = mSQLiteOpenHelper.getReadableDatabase();
 
-        Cursor cursor = testDb.rawQuery("PRAGMA table_info("+MovieContract.MovieEntry.TABLE_NAME+")", null);
+        Cursor cursor = testDb.rawQuery("PRAGMA table_info(" + MovieContract.MovieEntry.TABLE_NAME + ")", null);
         assertTrue("Error: There is no info for table " + MovieContract.MovieEntry.TABLE_NAME, cursor.moveToFirst());
 
         int columnNameIndex = cursor.getColumnIndex("name");
-        do{
+        do {
             columns.remove(cursor.getString(columnNameIndex));
         }
-        while(cursor.moveToNext());
+        while (cursor.moveToNext());
         assertTrue("Error: Movie table does not contain one or more column", columns.isEmpty());
     }
 }
