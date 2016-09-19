8a1f548cccf2ca443d8aec191371122f0065542a
Casak123 <casak123@gmail.com>
2016-09-19 12:30:50 +0300

    Tests refactor

--- app/src/androidTest/java/ru/casak/IMDB_searcher/providers/MovieDbHelperTest.java
+++ app/src/androidTest/java/ru/casak/IMDB_searcher/providers/MovieDbHelperTest.java
@@ -9,6 +9,7 @@ import android.test.RenamingDelegatingContext;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -33,9 +34,16 @@ public class MovieDbHelperTest {
 
     @BeforeClass
     public static void setUp() throws JSONException {
-        mContext = new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "test_");
+        mContext = InstrumentationRegistry.getTargetContext();
+        mContext.deleteDatabase(mSQLiteOpenHelper.getDatabaseName());
         mSQLiteOpenHelper = new MovieDbHelper(mContext);
         //mMovieJSON = new JSONObject("{   \"adult\": false,   \"backdrop_path\": \"/hNFMawyNDWZKKHU4GYCBz1krsRM.jpg\",   \"belongs_to_collection\": null,   \"budget\": 63000000,   \"genres\": [     {       \"id\": 18,       \"name\": \"Drama\"     }   ],   \"homepage\": \"\",   \"id\": 550,   \"imdb_id\": \"tt0137523\",   \"original_language\": \"en\",   \"original_title\": \"Fight Club\",   \"overview\": \"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \\\"fight clubs\\\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.\",   \"popularity\": 2.50307202280779,   \"poster_path\": \"/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg\",   \"production_companies\": [     {       \"name\": \"20th Century Fox\",       \"id\": 25     },     {       \"name\": \"Fox 2000 Pictures\",       \"id\": 711     },     {       \"name\": \"Regency Enterprises\",       \"id\": 508     }   ],   \"production_countries\": [     {       \"iso_3166_1\": \"DE\",       \"name\": \"Germany\"     },     {       \"iso_3166_1\": \"US\",       \"name\": \"United States of America\"     }   ],   \"release_date\": \"1999-10-14\",   \"revenue\": 100853753,   \"runtime\": 139,   \"spoken_languages\": [     {       \"iso_639_1\": \"en\",       \"name\": \"English\"     }   ],   \"status\": \"Released\",   \"tagline\": \"How much can you know about yourself if you've never been in a fight?\",   \"title\": \"Fight Club\",   \"video\": false,   \"vote_average\": 7.7,   \"vote_count\": 3185 }");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (mSQLiteOpenHelper != null)
+            mSQLiteOpenHelper.close();
     }
 
     @Test
